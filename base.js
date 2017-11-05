5e712564c52c023d4eafd9676e81adff40c754a7
valkynaz <valkynaz_wow@live.co.uk>
2017-11-05 22:54:42 +0000

    Update base.js

--- base.js
+++ base.js
@@ -1,127 +1,147 @@
-/* VARIABLES SECTION */
+var canvas = document.querySelector("canvas"); //searches HTML documents for "canvas" tag
+var c = canvas.getContext("2d"); //"context" in HTML Canvas is shortened to "c". "paint brush"/allows access to variables to draw 2d shapes
 
-/* PLAYER STATS AND INFORMATION */
+canvas.width = window.innerWidth; //sets canvas width to browser inner width
+canvas.height = window.innerHeight; //sets canvas height to browser inner height
+
+
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function distance(x1, y1, x2, y2) {
+    const xDist = x2 - x1
+    const yDist = y2 - y1
+
+    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+}
+
+// (var i = 0; i < 50; i++){
+
 
 var realm = {
-	current_dust: 1,
-	lifetime_dust: 1,
-	current_energy: 1,
-	lifetime_energy: 1,
-	current_clicks: 1,
-	lifetime_clicks: 1,
-	lifetime_solar_mass: 1,
-	time_played_this_realm: 1,
-	date_started_playing: 1,
-	time_since_started_playing: 1, //in seconds
-	upgrades_purchased: 1,
-	formation_count: 1,
-	click_object_click_income: 1, //income is solar mass divided by dust per solar mass
-	click_object_solar_mass: 1,
-	click_object_solar_mass_increase_dust_cost: 1,
-	click_object_solar_mass_increase_dust_cost_multiplier: 1.15,
-	click_object_click_income_per_solar_mass: 1, //upgrade required to unlock more income per solar mass bonus
-	click_object_click_income_upgrade_level: 1, //upgrade essentially means more dust per click per solar mass
-	click_object_click_income_upgrade_cost: 25,
-	click_object_click_income_upgrade_cost_multiplier: 1.15,
-	click_object_dust_income_per_second: 1,
-	click_object_dust_income_per_solar_mass: 1,
-	click_object_energy_multiplier: 1.15,
-	click_object_energy_upgrade_level: 1,
-	click_object_energy_upgrade_cost: 1,
-	click_object_energy_upgrade_cost_multiplier: 1.15,
-	click_object_star_formed: 0, //0 = no, 1 = yes
-	click_object_star_formation_dust_required: 1,
-	click_object_black_hole_formed: 0, //0 = no, 1 = yes
-	click_object_black_hole_formation_dust_required: 1	
+	
+	clickObjectSolarMass: 75,
+	
+	asteroidArray: [],
+	
+	asteroidMaxRadius: 2,
+	asteroidMinRadius: 1,
+	asteroidColorArray: ["#5a554c", "#767676", "#93928c", "#362d01", "#9fd344"] // CREDIT TO PYRA_LIRA (SOURCED FROM http://www.color-hex.com/color-palette/21597)
 };
 
 
-/* dust can be converted into other currencies for upgrades e.g. energy/ dark matter/ dark energy or used to increase mass of object!!!
-
-galaxies compromised of different percentage of star clasfifications
-galaxies extremely hard to buy
-explode stars 
-
-upgrade to form galaxy once enough mass (gains status over time?)
-
-stars form over time
-gather dust to galaxy
-upgrades increase dust collection?
-
-supernova explosions (maybe accurate % per second?) that give energy
-
-e.g. 1% explode every second once at black hole stage
 
 
+window.addEventListener("resize", function() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 
-clicks increase amount of dust or size/strength of black hole?
-start off with galaxy/black hole
-dust makes stars after collecting so much
-upgrades to increase dust collection or size/strength of black hole
-
-
-as blackhole mass/gravity increases attracts more dust/stars and eventually a black hole
-
-start off with a star, explode to make a black hole, on reset star starts off with more mass or more elements etc
-*/
+	init();
+});
 
 
-/* FUNCTIONS */
+function Asteroid(x, y, dx, dy, radius, velocity) {
+	
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.radius = radius;
+	this.color = realm.asteroidColorArray[Math.floor(Math.random() * realm.asteroidColorArray.length)];
+	this.radians = Math.random() * Math.PI * 2;
+	this.velocity = velocity;
+	this.distanceFromClickObject = randomIntFromRange(250, 400);
+	
+	this.update = () => {
+		
+		var lastPoint = {x: this.x, y: this.y}
+		
+		// MOVE POINTS OVER TIME
+		this.radians += this.velocity;
+		this.x = x + Math.cos(this.radians) * this.distanceFromClickObject;
+		this.y = y + Math.sin(this.radians) * this.distanceFromClickObject;
+	
+		this.draw(lastPoint);
+	};
+	this.draw = lastPoint => {
+		
+	var oCanvasoContext =
+	var 
+	c.beginPath();
+	//c.globalAlpha = 1;
+	c.strokeStyle = this.color;
+	c.lineWidth = this.radius;
+	c.moveTo(lastPoint.x, lastPoint.y);
+	c.lineTo(this.x, this.y);
+	c.stroke();
+	c.fill();
+	c.closePath();
 
-function clickObjectClick(){
-	realm.current_dust = realm.current_dust + (realm.click_object_click_income + (realm.click_object_click_income_per_solar_mass * realm.click_object_solar_mass)); //adds dust income to current dus
-    realm.lifetime_dust = realm.current_dust; //adds dust income to lifetime dust
-	realm.current_clicks = realm.current_clicks + 1; //adds 1 to current clicks
-	realm.lifetime_clicks = realm.lifetime_clicks + 1; //adds 1 to lifetime clicks
-					updateDisplay();
+	};
+	
 }
 
-function dustIncome(){
-	realm.click_object_dust_income_per_second = (realm.click_object_solar_mass * realm.click_object_dust_income_per_solar_mass);
-	realm.current_dust = realm.current_dust + realm.click_object_dust_income_per_second;
+//var x = Math.random() * innerWidth;
+//var y = Math.random() * innerHeight;
+//var dx = Math.random() - 0.5;
+//var dy = Math.random() - 0.5;
+//var radius = 5;
+
+
+
+
+console.log(realm.asteroidArray);
+
+function init() {
 	
-	realm.time_since_started_playing = realm.time_since_started_playing + 1;
+	realm.asteroidArray = [];
+	
+	for (var i = 0; i < 500; i++) {
+
+	var x = innerWidth / 2;
+	var y = innerHeight / 2;
+	var dx = (Math.random() - 0.5) * 1;
+	var dy = (Math.random() - 0.5) * 1;
+	var radius = Math.random() + 2;
+	var velocity = randomIntFromRange(1, 2) / 2000;
 	
 
-	updateDisplay();
+	realm.asteroidArray.push(new Asteroid(x, y, dx, dy, radius, velocity));
+	var circle = new Asteroid(200, 200, 3, 3);
+	
+	}
+
+
+	
+	
+	
+	
 }
 
-// UPDATE DISPLAY
+function animate(){
+	requestAnimationFrame(animate);
+	c.fillStyle = "rgba(48, 60, 77, 0.1)";	
+	c.fillRect(0, 0, canvas.width, canvas.height);
+	
+	c.beginPath();
+	c.arc(innerWidth / 2, innerHeight / 2, realm.clickObjectSolarMass, 0, Math.PI * 2, false);
+	c.strokeStyle = "white";
+	c.stroke();
+	c.fillStyle = "white";
+	c.fill();
+	
+	for (var i = 0; i < realm.asteroidArray.length; i++){
+		realm.asteroidArray[i].update();
+	}
+	
+	realm.asteroidArray.forEach(asteroid => {
+		asteroid.update();
+	});
 
-function updateDisplay(){
-	
-	document.getElementById('clickObjectClickIncome').innerHTML = realm.click_object_click_income;
-	document.getElementById('clickObjectClickIncomePerSolarMass').innerHTML = realm.click_object_click_income_per_solar_mass;
-	document.getElementById('clickObjectClickIncomeUpgradeLevel').innerHTML = realm.click_object_click_income_upgrade_level;
-	document.getElementById('clickObjectClickIncomeUpgradeCost').innerHTML = realm.click_object_click_income_upgrade_cost;
-	
-	document.getElementById('currentDust').innerHTML = realm.current_dust;
-	document.getElementById('lifetimeDust').innerHTML = realm.lifetime_dust;
-	document.getElementById('currentEnergy').innerHTML = realm.current_energy;
-	document.getElementById('lifetimeEnergy').innerHTML = realm.lifetime_energy;
-	document.getElementById('lifetimeSolarMass').innerHTML = realm.lifetime_solar_mass;
-	document.getElementById('currentClicks').innerHTML = realm.current_clicks;
-	document.getElementById('lifetimeClicks').innerHTML = realm.lifetime_clicks;
-	document.getElementById('timePlayedThisRealm').innerHTML = realm.time_played_this_realm;
-	document.getElementById('timeSinceStartedPlaying').innerHTML = realm.time_since_started_playing;
-	document.getElementById('upgradesPurchased').innerHTML = realm.upgrades_purchased;
-	document.getElementById('formationCount').innerHTML = realm.formation_count;
-}
-	
-// SAVE GAME
-
-function save_game(){
-   localStorage['1'] = btoa(JSON.stringify(realm));
 }
 
-function load_game(){
-	if (!localStorage['1']) return;
-	var save_data = JSON.parse(atob(localStorage['1']));
-	realm = save_data;
-	updateDisplay();
-}
+init();
+animate();
 
-window.setInterval(function(){
-dustIncome();
-updateDisplay();
-}, 1000);
+
